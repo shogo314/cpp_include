e72416a350705dba938def0a13ca4ba0b7a0508d
shogo314 <shg.kawasome@gmail.com>
2024-02-20 11:39:45 +0900

    update type

--- sh-library/base.hpp
+++ sh-library/base.hpp
@@ -4,25 +4,24 @@
 #pragma GCC optimize("O3")
 #pragma GCC optimize("unroll-loops")
 
-using namespace std;
 using ll = long long;
 using ull = unsigned long long;
-using str = string;
-using pl = pair<ll, ll>;
+using str = std::string;
+using pl = std::pair<ll, ll>;
 template <typename T>
-using ml = map<ll, T>;
-using mll = map<ll, ll>;
-using sl = set<ll>;
+using ml = std::map<ll, T>;
+using mll = std::map<ll, ll>;
+using sl = std::set<ll>;
 using ld = long double;
-using pd = pair<ld, ld>;
+using pd = std::pair<ld, ld>;
 template <typename T>
-using vec = vector<T>;
+using vec = std::vector<T>;
 template <typename T>
-using vv = vector<vector<T>>;
+using vv = vec<vec<T>>;
 template <typename T>
-using vvv = vector<vector<vector<T>>>;
+using vvv = vec<vec<vec<T>>>;
 template <typename T1, typename T2>
-using vp = vector<pair<T1, T2>>;
+using vp = vec<std::pair<T1, T2>>;
 using vl = vec<ll>;
 using vvl = vv<ll>;
 using vvvl = vvv<ll>;
@@ -31,17 +30,21 @@ using vc = vec<char>;
 using vi = vec<int>;
 using vb = vec<bool>;
 using vpl = vec<pl>;
-using spl = set<pl>;
+using vvpl = vv<pl>;
+using spl = std::set<pl>;
 using vd = vec<ld>;
 using vpd = vec<pd>;
 template <typename T, int N>
-using ary = array<T, N>;
+using ary = std::array<T, N>;
 template <int N>
-using al = array<ll, N>;
+using al = std::array<ll, N>;
 template <int N1, int N2>
-using aal = array<array<ll, N2>, N1>;
+using aal = ary<ary<ll, N2>, N1>;
 template <int N>
 using val = vec<al<N>>;
+template <int N>
+using sal = set<al<N>>;
+using std::deque;
 
 #define all(obj) (obj).begin(), (obj).end()
 
@@ -71,18 +74,19 @@ inline bool chmax(T1 &a, T2 b) {
     return false;
 }
 
-#define LL(x) ll x;cin >> x;
-#define VL(a,n) vl a(n);cin >> a;
-#define AL(a,k) al<k> a;cin >> a;
-#define VC(a,n) vc a(n);cin >> a;
-#define VS(a,n) vs a(n);cin >> a;
-#define STR(s) str s;cin >> s;
-#define VPL(a,n) vpl a(n);cin >> a;
-#define VAL(a,n,k) val<k> a(n);cin >> a;
-#define VVL(a,n,m) vvl a(n,vl(m));cin >> a;
-#define SL(a,n) sl a;{VL(b,n);a=sl(all(b));}
+// #define LL(x) ll x;cin >> x;
+// #define VL(a,n) vl a(n);cin >> a;
+// #define AL(a,k) al<k> a;cin >> a;
+// #define VC(a,n) vc a(n);cin >> a;
+// #define VS(a,n) vs a(n);cin >> a;
+// #define STR(s) str s;cin >> s;
+// #define VPL(a,n) vpl a(n);cin >> a;
+// #define VAL(a,n,k) val<k> a(n);cin >> a;
+// #define VVL(a,n,m) vvl a(n,vl(m));cin >> a;
+// #define SL(a,n) sl a;{VL(b,n);a=sl(all(b));}
+#define IN(a,x) a x; std::cin >> x;
 
 template <typename T>
-using heap_max = priority_queue<T, vec<T>, less<T>>;
+using heap_max = std::priority_queue<T, vec<T>, less<T>>;
 template <typename T>
-using heap_min = priority_queue<T, vec<T>, greater<T>>;
+using heap_min = std::priority_queue<T, vec<T>, greater<T>>;
